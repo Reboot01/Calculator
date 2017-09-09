bf2b970b21073c910ff1fcc462e47ce082fd30f2
Reboot01 <christian.eanes@gmail.com>
2017-09-09 15:11:21 -0400

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Would you like to use this calculator? [Y/N]");
+            string input = Console.ReadLine();
+
+            while (input == "y")
+            {
+                int num1 = 0;
+                int num2 = 0;
+
+                bool isValidNum = false;
+
+                while (!isValidNum)
+                {
+                    Console.Write("Enter your number: ");
+                    input = Console.ReadLine();
+
+                    isValidNum = Int32.TryParse(input, out num1);
+
+                    if (!isValidNum)
+                    {
+                        Console.WriteLine("That is not a Number!");
+                    }
+                }
+                while (!isValidNum)
+                {
+                    Console.Write("Enter your number: ");
+                    input = Console.ReadLine();
+
+                    isValidNum = Int32.TryParse(input, out num2);
+
+                    if (!isValidNum)
+                    {
+                        Console.WriteLine("That is not a Number!");
+                    }
+                }
+
+                    Console.WriteLine(num1 + num2);
+
+                Console.ReadKey(true);
+                
+            }
+
+        }
+    }
+}
